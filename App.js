630a002494cf255faf7435669121c8ac34162054
Jv Orais <oraisjv@gmail.com>
2019-01-02 03:46:41 +0800

    UI added

--- App.js
+++ App.js
@@ -7,23 +7,49 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
+/*
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+*/
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Image 
+            style={styles.logo}
+            source={require('./Logo.png')}
+          />
+        </View>
+        <View style={styles.inputs}>
+          <Text style={styles.inputText}>Email</Text>
+          <TextInput
+            placeholder="Input Email Address"
+            placeholderTextColor="grey"
+            style={styles.input}
+          />
+          <Text style={styles.inputText}>Password</Text>
+          <TextInput
+            placeholder="Input Password"
+            placeholderTextColor="grey"
+            style={styles.input}
+          />
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+        </View>
+        {/* 7:46<View style={styles.formContainer}></View>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{instructions}</Text>*/}
       </View>
     );
   }
@@ -31,11 +57,59 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
+    padding:5,
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    //backgroundColor: 'blue'
   },
+  inputs: {
+    padding:10,
+    width: '100%',
+    position: 'absolute',
+    bottom:0,
+    //backgroundColor: 'yellow'
+
+  },
+  buttonContainer: {
+    backgroundColor: '#714db2',
+    paddingVertical: 15,
+    height: 45,
+    width: '100%',
+    marginBottom: 10
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+  logoContainer: {
+    //backgroundColor: 'green',
+
+  },
+  logo: {
+    
+
+  },
+  inputText: {
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+    color: 'black'
+    
+
+  },
+  input: {
+    fontStyle: 'italic',
+    width: '100%',
+    height: 45,
+    borderColor: '#8665c4',
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    marginBottom: 20,
+
+  },
+
+/*  
   welcome: {
     fontSize: 20,
     textAlign: 'center',
@@ -46,4 +120,6 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+*/
+
 });
