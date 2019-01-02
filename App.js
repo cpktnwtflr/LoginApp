5122c63446e6bbbf1545dccb5507468cec2225f3
Jv Orais <oraisjv@gmail.com>
2019-01-02 11:31:08 +0800

    Validation: empty field and password length added

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, Keyboard, Alert} from 'react-native';
 
 /*
 const instructions = Platform.select({
@@ -20,9 +20,29 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { email:'', password:''};
+  }
+  myFunction=() =>{ const {email, password} = this.state;
+  if(email == ""){
+    this.setState({Error: 'Email address field is empty.'});
+  }
+  else if(password == ""){
+    this.setState({Error: 'Password field is empty.'});
+  }
+  else if(password.length<6 || password.length>12){
+    // else if(password.length<6 && password.length>12){
+    this.setState({Error: 'Password length must be 6-12 characters'});
+  }
+  else{
+    alert('Login Successful!');
+  }
+  Keyboard.dismiss();
+  }
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView behaivior="padding" style={styles.container}>
         <View style={styles.logoContainer}>
           <Image 
             style={styles.logo}
@@ -34,15 +54,29 @@ export default class App extends Component<Props> {
           <TextInput
             placeholder="Input Email Address"
             placeholderTextColor="grey"
+            onSubmitEditing={() => this.passwordInput.focus()}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            returnKeyType="next"
             style={styles.input}
+            onChangeText={ email => this.setState({email}) }
           />
+          <Text style={styles.err}>{this.state.Error}</Text>
           <Text style={styles.inputText}>Password</Text>
           <TextInput
             placeholder="Input Password"
             placeholderTextColor="grey"
+            returnKeyType="done"
+            secureTextEntry
+            autoCapitalize="none"
+            autoCorrect={false}
             style={styles.input}
+            ref={(input) => this.passwordInput = input}
+            onChangeText={ password => this.setState({password}) }
           />
-          <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.err}>{this.state.Error}</Text>
+          <TouchableOpacity onPress={this.myFunction} style={styles.buttonContainer}>
             <Text style={styles.buttonText}>Sign In</Text>
           </TouchableOpacity>
         </View>
@@ -50,7 +84,7 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>*/}
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -62,50 +96,57 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     //backgroundColor: 'blue'
   },
+  err: {
+    color:'red', 
+    alignItems:'flex-start',
+    fontSize: 10
+  },
   inputs: {
     padding:10,
     width: '100%',
     position: 'absolute',
     bottom:0,
-    //backgroundColor: 'yellow'
+    backgroundColor: 'white'
 
   },
   buttonContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#714db2',
     paddingVertical: 15,
-    height: 45,
+    height: 40,
     width: '100%',
     marginBottom: 10
   },
   buttonText: {
     textAlign: 'center',
+    fontSize:20,
     fontWeight: 'bold'
+
   },
   logoContainer: {
     //backgroundColor: 'green',
 
   },
   logo: {
-    
 
   },
   inputText: {
     fontWeight: 'bold',
     alignSelf: 'flex-start',
     color: 'black'
-    
 
   },
   input: {
     fontStyle: 'italic',
     width: '100%',
-    height: 45,
+    height: 40,
     borderColor: '#8665c4',
     borderLeftWidth: 1,
     borderRightWidth: 1,
     borderTopWidth: 1,
     borderBottomWidth: 1,
-    marginBottom: 20,
+    
 
   },
 
